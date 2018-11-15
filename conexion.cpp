c0d8eee6907c48e416b27bddc0114ba0844a58b3
AMargery <38414315+AMargery@users.noreply.github.com>
2018-11-15 08:19:00 -0600

    Update conexion.cpp

--- conexion.cpp
+++ conexion.cpp
@@ -4,11 +4,12 @@
 
 #include "neurona.h"
 
-Conexion::Conexion(Neurona nNeurona, float nPeso)
+Conexion::Conexion(Neurona* nNeuronaR, float nPeso)
 {
     srand (time(NULL));
     float floatAleatorio, aleatorio;
     aleatorio = ( float )rand() / RAND_MAX;
     floatAleatorio = 0 + aleatorio * (1 - 0);
     peso = floatAleatorio;
+    receptora = nNeuronaR;
 }
