3775da6e26bbc83c90ba23cbd912db2f834f109c
AMargery <38414315+AMargery@users.noreply.github.com>
2018-11-15 18:14:59 -0600

    Update red.cpp

--- red.cpp
+++ red.cpp
@@ -1,12 +1,19 @@
 #include "red.h"
 #include <iostream> //para probar el imprimir
-using namespace std; //para probar el imprimir
+#include <cstdlib>
+#include <time.h>
+using namespace std; 
 
 Red::Red(int nCapas, vector<int> nNeuronas)
 {
     capas = nCapas;// por el momento solo se usa para poder imprimir
     neuronas = nNeuronas;// por el momento solo se usa para poder imprimir
-    unsigned int cntConexiones = capas * neuronas;
+    unsigned int cntConexiones;
+    for(int i = 0; i < capas; i++){
+        for(int i = 0; i < neuronas.size(); i++){
+            cntConexiones++;   
+        }
+    }
     vector< vector< int > > n(cntConexiones, vector<int>(cntConexiones, 0));
     matrizConexiones = n;
     for(int i = 0; i < nCapas; i++){
@@ -16,13 +23,18 @@ Red::Red(int nCapas, vector<int> nNeuronas)
             listaCapas.push_back(Capa(nNeuronas[i], 0));
         }
     }
-    /*
+    
+    float floatAleatorio, aleatorio;
+    srand( time( NULL ) );
     for(int i = 0; i < nCapas; i++){
-        for(int j = 0; i < nNeuronas[i]; i++){
-            listaCapas[i].listaNeuronas[j].AgregarConexion(listaCapas[i+1].listaNeuronas[j] ,);
+        for(int j = 0; j < nNeuronas[i]; j++){
+            for(int k = 0; k < nNeuronas[i + 1]; k++){
+                aleatorio = ( float )rand() / RAND_MAX;
+                floatAleatorio = 0 + aleatorio * (1 - 0);
+                listaCapas[i].listaNeuronas[j].AgregarConexion(&listaCapas[i+1].listaNeuronas[j], floatAleatorio);    
+            }
         }
     }
-    */
 }
 
 Red::~Red(){}
@@ -30,7 +42,12 @@ Red::~Red(){}
 //para visualizar en realidad no nos sirve para nada
 void Red::ImprimirMatriz()
 {
-    int cntConexiones = capas * neuronas;
+    unsigned int cntConexiones = 0;
+    for(int i = 0; i < capas; i++){
+        for(int i = 0; i < neuronas.size(); i++){
+            cntConexiones++;   
+        }
+    }
     for(int i = 0  ; i < cntConexiones ; ++i){
         for(int j = 0 ; j < cntConexiones ; ++j){
             cout << matrizConexiones[i][j];
