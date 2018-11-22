40dc3736cd7a5998753669b22ee77a2b5ec6b09a
Marcomora99 <39306176+Marcomora99@users.noreply.github.com>
2018-11-22 02:45:08 -0600

    Update codificadorletras.h

--- codificadorletras.h
+++ codificadorletras.h
@@ -1,19 +1,22 @@
-
 #ifndef CODIFICADORLETRAS_H
 #define CODIFICADORLETRAS_H
 
 #include <vector>
-#include <QString>
-#include <QFile>
-using namespace std;
+#include <fstream>
+#include <QtWidgets>
+#include <iostream>
+#include <sstream>
+#include<bits/stdc++.h>
 
+using namespace std;
 
 class codificadorLetras
 {
 public:
     codificadorLetras();
     vector<string> v;
-    void analizarArchivo();
+    float analizarArchivo(string nombreArchivo);
+    QString quitarSignosPuntuacion(QString texto);
 };
 
 #endif // CODIFICADORLETRAS_H
