44ab4e8261b5af8532e2d368bcc3181a94fbd3a6
Marcomora99 <39306176+Marcomora99@users.noreply.github.com>
2018-11-22 02:55:01 -0600

    Update codificadorletras.cpp

--- codificadorletras.cpp
+++ codificadorletras.cpp
@@ -16,24 +16,37 @@ float codificadorLetras::analizarArchivo(string nombreArchivo){
         cout << "Unable to open file datafile.txt";
     }else{
         while(getline (inFile,linea)) {
-           texto << linea << " ";
+           texto << linea << " ";  //lee el archivo y le manda linea por linea al stringstream texto
         }
     }
 
     inFile.close();
-    texto_str = texto.str();
-    transform(texto_str.begin(), texto_str.end(), texto_str.begin(), ::tolower);
-    QString texto_qstr = QString::fromStdString(texto_str);
-    texto_qstr = quitarSignosPuntuacion(texto_qstr);
-    QStringList arregloLetra = texto_qstr.split(" ");
-    QStringList arregloLetraSinRepetidas = arregloLetra;
-    arregloLetraSinRepetidas.removeDuplicates();
-    texto_qstr = arregloLetra.join(" ");
+    
+    texto_str = texto.str(); //Transforma el stringstream a stream
+    
+    transform(texto_str.begin(), texto_str.end(), texto_str.begin(), ::tolower); // Hace todas las letras minusculas
+    
+    QString texto_qstr = QString::fromStdString(texto_str); // Crea un QString con la letra para poder trabajarla de una mejor manera
+    
+    texto_qstr = quitarSignosPuntuacion(texto_qstr); //Quita signos de puntuacion para no contar mas palabras de la cuenta
+    
+    QStringList arregloLetra = texto_qstr.split(" "); // Crea una lista con cada palabra de la cancion
+    
+    QStringList arregloLetraSinRepetidas = arregloLetra; // Crea otra lista con el objetivo de quitarle las palabras repetidas
+    
+    arregloLetraSinRepetidas.removeDuplicates(); // Quita de la lista las palabras repetidas para tener el total de las palabras que aparecen en la letra
+    
+    texto_qstr = arregloLetra.join(" ");  // Une la lista de todas las palabras en un solo QString para imprimirlo
+    
     cout << texto_qstr.toStdString();
+    
     cout << endl << endl;
-    texto_qstr = arregloLetraSinRepetidas.join(" ");
+    
+    texto_qstr = arregloLetraSinRepetidas.join(" "); // Une la lista de todas las palabras sin repeticion en un solo QString para imprimirlo
+    
     cout << texto_qstr.toStdString();
 
+    //Analisis de las mas repetidas
     /*for(int i=0 ; i < arregloLetraSinRepetidas.size(); i++){
         for(int j = 0; j < arregloLetra.size();j++){
             if(arregloLetraSinRepetidas.at(i)==arregloLetra.at(j)){
